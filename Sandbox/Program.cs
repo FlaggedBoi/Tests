d480f77c834061a8bca4872ffbe6621e2afa9a54
Владислав Никитин <dushekrad@gmail.com>
2018-12-19 22:04:04 +0400

    Sort

---
+++ Sandbox/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // ввод чисел
+            int[] nums = new int[7];
+            Console.WriteLine("Введите семь чисел");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.Write("{0}-е число: ", i + 1);
+                nums[i] = Int32.Parse(Console.ReadLine());
+            }
+
+            // сортировка
+            int temp;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        temp = nums[i];
+                        nums[i] = nums[j];
+                        nums[j] = temp;
+                    }
+                }
+                for (int b = 0; b < nums.Length; b++)
+                {
+                    Console.WriteLine(nums[b]);
+                }
+            }
+
+            // вывод
+            Console.WriteLine("Вывод отсортированного массива");
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Console.WriteLine(nums[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
